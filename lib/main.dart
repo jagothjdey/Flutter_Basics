ce4b159704e72de19fa092539b4a93e97906d2e0
dey <jagothjyotidey91@gmail.com>
2020-07-31 18:31:37 +0200

    appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Flutter App'),
+        ),
+        body: Text('This is my default body text'),
+      ),
     );
   }
 }
