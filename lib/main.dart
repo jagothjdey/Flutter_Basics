f5859b11e19542d29dfa2b87c7c75d2a560432db
dey <jagothjyotidey91@gmail.com>
2020-08-01 13:43:54 +0200

    Private properties added

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,59 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestionFirst() {
+    setState(() {
+      _questionIndex = 0;
+    });
+  }
+
+  void _answerQuestionSecond() {
+    setState(() {
+      _questionIndex = 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your mother name',
+      'What\'s your father name',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First Flutter App'),
         ),
-        body: Text('This is my default body text'),
+        body: Column(
+          children: [
+            Text(
+              questions[_questionIndex],
+            ),
+            Column(
+              children: [
+                RaisedButton(
+                  child: Text(questions[0]),
+                  onPressed: _answerQuestionFirst,
+                ),
+                RaisedButton(
+                  child: Text(questions[1]),
+                  onPressed: _answerQuestionSecond,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
