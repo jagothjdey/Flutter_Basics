0e3a21d7f493db92e4720b277042eb00495d55d0
dey <jagothjyotidey91@gmail.com>
2020-08-02 14:53:28 +0200

    done

--- lib/result.dart
+++ lib/result.dart
@@ -2,16 +2,29 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int finalScore;
+  final Function restartQuiz;
 
-  Result(this.finalScore);
+  Result(this.finalScore, this.restartQuiz);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        finalScore.toString(),
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 30),
+      child: Column(
+        children: [
+          Text(
+            'Your Final Score is : $finalScore',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+          ),
+          FlatButton(
+            onPressed: restartQuiz,
+            child: Text(
+              'Restart Quiz',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+            ),
+          )
+        ],
       ),
     );
   }
